abf69781173df6aa44308fbdf44c0109c04601a1
Han Bui <ha@sparkenv.com>
2025-01-12 17:58:10 +0100

    added lexical analysis (in language analysis folder) - also renamed the directory for readability

---
+++ shuffle_chosen_responses.py
@@ -0,0 +1,35 @@
+import os
+import json
+import random
+
+def load_json_files_from_folder(folder_path):
+    continuations = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".json"):
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, 'r', encoding='utf-8') as f:
+                try:
+                    data = json.load(f)
+
+                    if 'top_responses' in data:
+                        continuations.extend([resp['continuation'] for resp in data['top_responses']])
+
+                    if 'lowest_responses' in data:
+                        continuations.extend([resp['continuation'] for resp in data['lowest_responses']])
+                except json.JSONDecodeError as e:
+                    print(f"Error reading {filename}: {e}")
+
+    return continuations
+
+folder_path = "./human_evaluation/"  
+continuations = load_json_files_from_folder(folder_path)
+
+
+# Shuffle the continuations
+random.shuffle(continuations)
+
+for cont in continuations:
+    print(cont)
+    print("-" * 80)
+
